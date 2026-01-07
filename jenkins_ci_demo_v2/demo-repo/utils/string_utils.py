4a075797a6abc23b7c408ddd2b914a20b5a4bc36
yoav31 <yoav31yv@gmail.com>
2026-01-07 09:17:05 +0200

    Delete jenkins_ci_demo_v2 directory

--- jenkins_ci_demo_v2/demo-repo/utils/string_utils.py
+++
@@ -1,5 +0,0 @@
-
-def normalize_name(name: str) -> str:
-    if not isinstance(name, str):
-        raise TypeError('name must be a string')
-    return ' '.join(part.capitalize() for part in name.strip().split())
